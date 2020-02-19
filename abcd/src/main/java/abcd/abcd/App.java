20be124bc16a68fd07e4e7c8a1136d39db2453f8
H2_9_19_User4 <H2_9_19_User4@172.31.37.149>
2020-02-19 10:17:17 +0000

    case study

---
+++ abcd/src/main/java/abcd/abcd/App.java
@@ -0,0 +1,13 @@
+package abcd.abcd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ abcd/src/test/java/abc/def.java
@@ -0,0 +1,10 @@
+package abc;
+
+import org.testng.annotations.Test;
+
+public class def {
+  @Test
+  public void f() {
+	  
+  }
+}
